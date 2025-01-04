18a6e35a78372164232cc19e066dafe7701e4cb8
Alex Avk <iwanov.vany-ivanov@yandex.ru>
2025-01-05 00:07:56 +0300

    Добавил контекст

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,26 @@
+import { MessageInstance } from 'antd/es/message/interface';
 import Button from './components/Button';
+import { createStrictContext } from './shared/lib/react';
+import { message } from 'antd';
+
+export const MessageContext = createStrictContext<{
+  messageApi: MessageInstance;
+}>();
 
 const App = () => {
+  const [messageApi, contextHolder] = message.useMessage();
+
   return (
-    <div>
-      <Button variant="primary">Primary</Button>
-      <Button>Default</Button>
-    </div>
+    <>
+      {contextHolder}
+
+      <MessageContext.Provider value={{ messageApi }}>
+        <div>
+          <Button variant="primary">Primary</Button>
+          <Button>Default</Button>
+        </div>
+      </MessageContext.Provider>
+    </>
   );
 };
 
--- ui/src/components/Button.tsx
+++ ui/src/components/Button.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { MessageContext } from 'src/App';
+import { useStrictContext } from 'src/shared/lib/react';
 
 interface ButtonProps extends React.ComponentProps<'button'> {
   children: React.ReactNode;
@@ -10,9 +12,12 @@ const Button: React.FC<ButtonProps> = ({
   variant = 'default',
   ...props
 }) => {
+  const { messageApi } = useStrictContext(MessageContext);
+
   return (
     <button
       {...props}
+      onClick={() => messageApi.success('hello')}
       style={{
         background: variant === 'primary' ? '#000' : '#fff',
         color: variant === 'primary' ? '#fff' : '#000',
